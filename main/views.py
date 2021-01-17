2244448c170d0860700258a4228029458ada35b7
unknown <altynay-2013@mail.ru git config --global user.name Altynai29>
2021-01-17 08:49:16 +0300

    init

---
+++ main/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, HttpResponse
+
+def homepage(request):
+    return HttpResponse("Hello world")
+
+
+def test(request):
+    return render(request, "test.html")
+
+
+
+
+# Create your views here.
